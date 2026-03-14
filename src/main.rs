83ef03ac5df8561752861fbba5bec257ceebc08f
evaporator <osxar.jeavons@gmail.com>
2026-03-14 23:25:57 +0000

    added binding port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("172.0.0.1:8080").unwrap();
+    println!("listening at 172.0.0.1:8080");
 }
