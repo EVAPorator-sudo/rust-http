db6feea842bf8e8341946813e378227da3a3edaa
EVAPorator <osxar.jeavons@gmail.com>
2026-03-15 21:29:36 +0000

    added skeleton of main class

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ use std::io::{Read, Write};
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => toDo(),
+            Err(e) => eprintln!("Connection rejected {e}", e),
+        }
+    }
 }
