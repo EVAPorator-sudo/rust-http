1b70dac8f91b5c0f67a24995945a5fdae609dad9
EVAPorator <osxar.jeavons@gmail.com>
2026-03-15 21:21:47 +0000

    added initial address binding

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
-use std::net::TcpStream;
+use std::io::{Read, Write};
 
 fn main() {
-    let listener = TcpListener::bind("172.0.0.1:8080").unwrap();
-    println!("listening at 172.0.0.1:8080");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server listening on port 8080");
 }
